61cf5f9a92c289e0c313fb82152cb019e95d4935
Blaine Bush <blaine.bush@magnaglobal.com>
2020-10-12 19:23:21 +0100

    Tweaks

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,11 +8,41 @@
 
 @section('content')
 
-<p>Parvifolium is the name of a number of plant species, including the <a href="https://en.wikipedia.org/wiki/Vaccinium_parvifolium">red huckleberry</a> (<em>Vaccinium parvifolium</em>) which is common in the Pacific Northwest. The name is derived from Latin <em>parvus</em>, meaning small, and <em>folium</em>, meaning leaf.</p>
+<h1>The Author</h1>
 
-<img class="inline-image" src="{{ asset('images/huckleberry_BW_1.png') }}" alt="A black-and-white sketch of the branch of a huckleberry bush">
+<p>
+    TBD.
+</p>
 
-<p><em>Folium</em> can also refer to the leaf of a book. In that sense, a small leaf is a bite-sized amount of reading. That will have to do, as to my knowledge there is no Latin word for blog.</p>
+<h1>The Name</h1>
+
+<p>
+    Parvifolium is the name of a number of plant species, including the
+    <a href="https://en.wikipedia.org/wiki/Vaccinium_parvifolium">red huckleberry</a> (<em>Vaccinium parvifolium</em>)
+    which is common in the Pacific Northwest.
+    The name is derived from Latin <em>parvus</em>, meaning small, and <em>folium</em>, meaning leaf.
+</p>
+
+<img class="inline-image" src="{{ asset('images/huckleberry_BW_1.png') }}" alt="">
+
+<p>
+    <em>Folium</em> can also refer to the leaf of a book.
+    In that sense, a small leaf is a bite-sized amount of reading.
+    That will have to do, as to my knowledge there is no Latin word for blog.
+</p>
+
+<h1>The Code</h1>
+
+<p>
+    This site is built with Laravel.
+    Some components, like the Whitehack character generator, use Vue.js. Rollable tables,
+    like you see in my overland Barrowmaze encounters post, use a little bit of vanilla JavaScript.
+    Generally, I try to lean on the basics as much as possible and let the content and design take center stage.
+</p>
+
+<p>
+    All of the source code for this website is available on my GitHub.
+</p>
 
 {{-- <p>Parvifolium is the website and playground of Blaine Bush.</p>
 
