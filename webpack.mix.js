9193268f956dd91a6c810b37b93872a6d9540d59
Blaine Bush <blaine.bush@magnaglobal.com>
2021-01-12 10:56:49 +0000

    Add Pusher/Echo config.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix
+    .js('resources/js/bootstrap.js', 'public/js')
     .scripts('resources/js/app.js', 'public/js/app.js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/login.scss', 'public/css')
