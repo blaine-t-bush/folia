930fa37123822ff1cc5a312769d821f77f140ee3
Blaine Bush <blaine.bush@magnaglobal.com>
2021-01-16 15:04:17 +0000

    Separated auth tests into login and register tests. Updated post tests.

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -8,63 +8,14 @@ use Tests\TestCase;
 
 use App\Http\Controllers\UserController;
 
-class AuthTest extends TestCase
+class LoginTest extends TestCase
 {
     use RefreshDatabase;
     use WithFaker;
 
     /** @test */
-    public function can_direct_to_registration() {
-        // Send get request to /register. Should see register form.
-        $response = $this->get('/register');
-        $response->assertStatus(200);
-        $response->assertSee('<form class="register"', false);
-    }
-
-    /** @test */
-    public function can_register_user() {
-        // Send post request to /register with complete and unique credentials. Should create a user.
-        $response = $this->post('/register', [
-            'user_id' => $this->faker->userName,
-            'display_name' => $this->faker->name,
-            'password' => $this->faker->password,
-        ]);
-
-        // Should redirect back to home page after registration.
-        $response->assertLocation('/');
-    }
-
-    /** @test */
-    public function cannot_register_incomplete() {
-        // Send post request to /register with incomplete credentials.
-        $response = $this->post('/register', [
-            'user_id' => null,
-            'display_name' => null,
-            'password' => null,
-        ]);
-
-        // Should throw validation errors.
-        $response->assertSessionHasErrors(['user_id', 'display_name', 'password']);
-    }
-
-    /** @test */
-    public function cannot_register_duplicate() {
-        // Create a new user.
-        $user = UserController::create($this->faker->userName, $this->faker->name, $this->faker->password);
-
-        // Send post request to /register with non-unique user_id. Should return to /register with errors.
-        $response = $this->post('/register', [
-            'user_id' => $user->id,
-            'display_name' => $this->faker->name,
-            'password' => $this->faker->password,
-        ]);
-
-        // Should throw validation error for user_id.
-        $response->assertSessionHasErrors(['user_id']);
-    }
-
-    /** @test */
-    public function can_direct_to_login() {
+    public function can_direct_to_login()
+    {
         // Send get request to /login. Should see login form.
         $response = $this->get('/login');
         $response->assertStatus(200);
@@ -72,7 +23,8 @@ class AuthTest extends TestCase
     }
 
     /** @test */
-    public function can_login_user() {
+    public function can_login_user()
+    {
         // Create a new user.
         $password = $this->faker->password;
         $user = UserController::create($this->faker->userName, $this->faker->name, $password);
@@ -91,7 +43,8 @@ class AuthTest extends TestCase
     }
 
     /** @test */
-    public function cannot_login_incomplete() {
+    public function cannot_login_incomplete()
+    {
         // Create a new user.
         $password = $this->faker->password;
         $user = UserController::create($this->faker->userName, $this->faker->name, $password);
@@ -110,7 +63,8 @@ class AuthTest extends TestCase
     }
 
     /** @test */
-    public function cannot_login_invalid() {
+    public function cannot_login_invalid()
+    {
         // Create a new user.
         $password = $this->faker->password;
         $user = UserController::create($this->faker->userName, $this->faker->name, $password);
--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -15,80 +15,243 @@ class PostTest extends TestCase
     use WithFaker;
     
     /** @test */
-    public function can_create_post() {
-        // Create a new user.
+    public function can_create_post()
+    {
+        // Create user.
+        $id = $this->faker->userName;
+        $display_name = $this->faker->name;
         $password = $this->faker->password;
-        $user = UserController::create($this->faker->userName, $this->faker->name, $password);
+        $user = UserController::create($id, $display_name, $password);
+        $user->refresh();
 
-        // Make a login request, starting from an empty session (i.e. ensure no prior logins).
+        // Log user in.
         $response = $this->withSession([])->post('/login', [
             'user_id' => $user->id,
             'password' => $password,
         ]);
 
-        // Verify no posts exists.
+        // Verify no posts already exist.
         $this->assertDatabaseCount('posts', 0);
 
-        // Attempt to create a post.
+        // Create post.
         $body = $this->faker->paragraph;
-        $response = $this->post('/posts', [
+        $response = $this->post('/api/posts', [
             'user_id' => $user->id,
             'body' => $body,
         ]);
 
-        // Verify that post now exists.
+        // Check response. Should return a PostResource.
+        $response
+            ->assertStatus(201)
+            ->assertJson([
+                'user_id' => $user->id,
+                'user' => [
+                    'display_name' => $user->display_name,
+                ],
+                'body' => $body,
+            ]);
+
+        // Check database for matching post.
+        $this->assertDatabaseCount('posts', 1);
         $this->assertDatabaseHas('posts', [
             'user_id' => $user->id,
             'body' => $body,
         ]);
-        $this->assertDatabaseCount('posts', 1);
     }
     
     /** @test */
-    public function can_delete_post() {
-        // Create a new user.
+    public function can_delete_post()
+    {
+        // Create user.
+        $id = $this->faker->userName;
+        $display_name = $this->faker->name;
         $password = $this->faker->password;
-        $user = UserController::create($this->faker->userName, $this->faker->name, $password);
+        $user = UserController::create($id, $display_name, $password);
+        $user->refresh();
 
-        // Make a login request, starting from an empty session (i.e. ensure no prior logins).
+        // Log user in.
         $response = $this->withSession([])->post('/login', [
             'user_id' => $user->id,
             'password' => $password,
         ]);
 
-        // Create a post.
+        // Create post.
         $body = $this->faker->paragraph;
-        $response = $this->post('/posts', [
+        $response = $this->post('/api/posts', [
             'user_id' => $user->id,
             'body' => $body,
         ]);
 
-        $post = Post::where('user_id', $user->id)->where('body', $body)->first();
+        $post_id = $response['id'];
 
-        // Delete the post.
-        $response = $this->delete('/posts', [
-            'post_id' => $post->id,
-            'user_id' => $user->id,
+        // Delete post.
+        $response = $this->delete('/api/posts', [
+            'id' => $post_id,
         ]);
 
+        // Check response. Should return a PostResource.
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'id' => $post_id,
+                'user_id' => $user->id,
+                'user' => [
+                    'display_name' => $user->display_name,
+                ],
+                'body' => $body,
+            ]);
+
+        // Check that matching post has been deleted from database.
         $this->assertDatabaseCount('posts', 0);
+        $this->assertDatabaseMissing('posts', [
+            'id' => $post_id,
+            'user_id' => $user->id,
+            'body' => $body,
+        ]);
     }
 
     /** @test */
-    public function can_delete_post_with_comments() {
-        // Create a post.
+    public function can_delete_post_with_comment_from_same_user()
+    {
+        // Create user.
+        $id = $this->faker->userName;
+        $display_name = $this->faker->name;
+        $password = $this->faker->password;
+        $user = UserController::create($id, $display_name, $password);
+        $user->refresh();
 
-        // Add comments.
+        // Log user in.
+        $response = $this->withSession([])->post('/login', [
+            'user_id' => $user->id,
+            'password' => $password,
+        ]);
 
-        // Attempt to delete post.
+        // Create post.
+        $body = $this->faker->paragraph;
+        $response = $this->post('/api/posts', [
+            'user_id' => $user->id,
+            'body' => $body,
+        ]);
+
+        $post_id = $response['id'];
+
+        // Add comment to post.
+        $response = $this->post('/api/comments', [
+            'user_id' => $user->id,
+            'body' => $this->faker->paragraph,
+        ]);
+
+        // Delete post.
+        $response = $this->delete('/api/posts', [
+            'id' => $post_id,
+        ]);
+
+        // Check response. Should return a PostResource.
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'id' => $post_id,
+                'user_id' => $user->id,
+                'user' => [
+                    'display_name' => $user->display_name,
+                ],
+                'body' => $body,
+            ]);
+
+        // Check that matching post has been deleted from database.
+        $this->assertDatabaseCount('posts', 0);
+        $this->assertDatabaseMissing('posts', [
+            'id' => $post_id,
+            'user_id' => $user->id,
+            'body' => $body,
+        ]);
     }
 
     /** @test */
-    public function can_delete_post_with_reactions() {
-        // Create a post.
+    public function can_delete_post_with_comment_from_other_users()
+    {
 
-        // Add reactions.
+    }
 
-        // Attempt to delete post.
+    /** @test */
+    public function can_delete_post_with_reactions_from_same_user()
+    {
+        // Create user.
+        $id = $this->faker->userName;
+        $display_name = $this->faker->name;
+        $password = $this->faker->password;
+        $user = UserController::create($id, $display_name, $password);
+        $user->refresh();
+
+        // Log user in.
+        $response = $this->withSession([])->post('/login', [
+            'user_id' => $user->id,
+            'password' => $password,
+        ]);
+
+        // Create post.
+        $body = $this->faker->paragraph;
+        $response = $this->post('/api/posts', [
+            'user_id' => $user->id,
+            'body' => $body,
+        ]);
+
+        $post_id = $response['id'];
+
+        // Add reactions to post.
+        $response = $this->post('/api/reactions', [
+            'user_id' => $user->id,
+            'post_id' => $post_id,
+            'type' => 'smile',
+        ]);
+        
+        $response = $this->post('/api/reactions', [
+            'user_id' => $user->id,
+            'post_id' => $post_id,
+            'type' => 'frown',
+        ]);
+        
+        $response = $this->post('/api/reactions', [
+            'user_id' => $user->id,
+            'post_id' => $post_id,
+            'type' => 'heart',
+        ]);
+        
+        $response = $this->post('/api/reactions', [
+            'user_id' => $user->id,
+            'post_id' => $post_id,
+            'type' => 'laugh',
+        ]);
+
+        // Delete post.
+        $response = $this->delete('/api/posts', [
+            'id' => $post_id,
+        ]);
+
+        // Check response. Should return a PostResource.
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'id' => $post_id,
+                'user_id' => $user->id,
+                'user' => [
+                    'display_name' => $user->display_name,
+                ],
+                'body' => $body,
+            ]);
+
+        // Check that matching post has been deleted from database.
+        $this->assertDatabaseCount('posts', 0);
+        $this->assertDatabaseMissing('posts', [
+            'id' => $post_id,
+            'user_id' => $user->id,
+            'body' => $body,
+        ]);
+    }
+
+    /** @test */
+    public function can_delete_post_with_reactions_from_other_users()
+    {
+
     }
 }
